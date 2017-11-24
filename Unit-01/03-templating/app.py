dd227074d2dbe6bd96400d8203ea0b9f9edc24c1
Trevor Hoy <trevorphoy@gmail.com>
2017-11-23 18:05:32 -0800

    adding part 1 solutions

--- Unit-01/03-templating/app.py
+++ Unit-01/03-templating/app.py
@@ -3,16 +3,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 #PART 1
-@app.route('/person')
-def person():
-  return 'Your name {name} and your age {age}'
+@app.route('/person/<name>/<int:age>')
+def person(name, age):
+	return render_template('person.html', name=name, age=age)
+
     
-
-
-#PART2
-# @app.route('/calculate')
-# def calculate():
-# 	pass
+# PART2
+@app.route('/calculate')
+def calculate():
+	pass
 
 if __name__ == '__main__':
 	app.run(debug=True)
