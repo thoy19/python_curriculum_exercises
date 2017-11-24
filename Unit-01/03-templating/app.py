8d71d3eece193c6b5deae2ec2a187c19a08bed92
Trevor Hoy <trevorphoy@gmail.com>
2017-11-24 10:29:23 -0800

    adding final changes to templating

--- Unit-01/03-templating/app.py
+++ Unit-01/03-templating/app.py
@@ -10,9 +10,25 @@ def person(name, age):
     
 # PART2
 @app.route('/calculate')
-def calculate(add, subtract, multiply, divide):
-	
+def calculate():
 	return render_template('calc.html')
+
+@app.route('/math', methods=['POST'])
+def math():
+	num1 = request.form.get('num1')
+	num2 = request.form.get('num2')
+	if request.form.get('calculation') == 'add':
+		value = int(num1) + int(num2)
+		return str(value)
+	elif request.form.get('calculation') == 'subtract':
+		value = int(num1) - int(num2)
+		return str(value)
+	elif request.form.get('calculation') == 'multiply':
+		value = int(num1) * int(num2)
+		return str(value)
+	elif request.form.get('calculation') == 'divide':
+		value = int(num1) / int(num2)
+		return str(value)
 
 if __name__ == '__main__':
 	app.run(debug=True)
