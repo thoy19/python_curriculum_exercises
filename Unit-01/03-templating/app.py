9fd9785f2f795ec5c46ca62c1a4818ae19670618
Trevor Hoy <trevorphoy@gmail.com>
2017-11-23 17:45:43 -0800

    small update

--- Unit-01/03-templating/app.py
+++ Unit-01/03-templating/app.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 #PART 1
-# @app.route('/show-form/<name>/<int:age>')
-# def show_form(name, age):
-# 	return f'Your name {name} and your age {age}'
-
+@app.route('/person')
+def person():
+  return 'Your name {name} and your age {age}'
+    
 
 
 #PART2
-@app.route('/calculate')
-def calculate():
-	pass
+# @app.route('/calculate')
+# def calculate():
+# 	pass
 
 if __name__ == '__main__':
 	app.run(debug=True)
