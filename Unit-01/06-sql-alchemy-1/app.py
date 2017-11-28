76f917da76fe66deb62a6cbf53d8cb8bf0623d1b
Trevor Hoy <trevorphoy@gmail.com>
2017-11-27 21:10:36 -0800

    adding everything but 404 error

--- Unit-01/06-sql-alchemy-1/app.py
+++ Unit-01/06-sql-alchemy-1/app.py
@@ -24,18 +24,15 @@ class Snacks(db.Model):
 		self.kind = kind
 
 
-snickers = Snacks(name='snickers', kind='candybar')
-eggs = Snacks(name='eggs', kind='candy')
-snack_list = [snickers, eggs]
-
 #SHOW ALL SNACKS
 @app.route('/snacks', methods=['GET','POST'])
 def index():
 	if request.method == 'POST':
-		snack_list.append(Snacks(request.form['snack'], request.form['kind']))
+		new_snack = Snacks(request.form['snack'], request.form['kind'])
+		db.session.add(new_snack)
+		db.session.commit()
 		return redirect(url_for('index'))
-
-	return render_template('index.html', snack_list=snack_list)
+	return render_template('index.html', snack_list=Snacks.query.all())
 
 #FORM FOR NEW SNACK
 @app.route('/snacks/new')
@@ -45,17 +42,18 @@ def new():
 #VIEW BY SNACK
 @app.route('/snacks/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def show(id):
-	for snack in snack_list:
-		if snack.id == id:
-			found_snack = snack
+	found_snack = Snacks.query.get(id)
 
 	if request.method == b'PATCH':
 		found_snack.name = request.form['snack']
 		found_snack.kind = request.form['kind']
+		db.session.add(found_snack)
+		db.session.commit()
 		return redirect(url_for('index'))
 
 	if request.method == b'DELETE':
-		snack_list.remove(found_snack)
+		db.session.delete(found_snack)
+		db.session.commit()
 		return redirect(url_for('index'))
 
 	return render_template('show.html', snack=found_snack)
@@ -63,14 +61,9 @@ def show(id):
 
 @app.route('/snacks/<int:id>/edit')
 def edit(id):
-	for snack in snack_list:
-		if snack.id == id:
-			found_snack = snack
+	found_snack = Snacks.query.get(id)
 	return render_template('edit.html', snack=found_snack)
 	
-
-
-
 
 if __name__ == '__main__':
 	app.run(debug=True)
