5b1ab41777f902fe9adf2bc8e5d2caeebb9b7b17
Trevor Hoy <trevorphoy@gmail.com>
2017-11-30 14:15:36 -0800

    users table created

--- Unit-01/07-sql-alchemy-2/users-app/app.py
+++ Unit-01/07-sql-alchemy-2/users-app/app.py
@@ -1,8 +1,36 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_modus import Modus
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/users-app-db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+modus = Modus(app)
+
+
+class User(db.Model):
+
+	__tablename__ = 'users'
+
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.Text)
+	last_name = db.Column(db.Text)
+
+	def __init__(self, first_name, last_name):
+		self.first_name = first_name
+		self.last_name = last_name
+
+
+@app.route('/')
+def root():
+	return redirect(url_for('index'))
+
+@app.route('/users', methods=['GET', 'POST'])
+def index():
+	pass	
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=3000)
