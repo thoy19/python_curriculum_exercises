bd25d7b1141fc82f8f65898fb25ff65db071636d
Trevor Hoy <trevorphoy@gmail.com>
2017-11-30 16:33:57 -0800

    added messages table

--- Unit-01/07-sql-alchemy-2/users-app/app.py
+++ Unit-01/07-sql-alchemy-2/users-app/app.py
@@ -17,10 +17,19 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	first_name = db.Column(db.Text)
 	last_name = db.Column(db.Text)
+	messages = db.relationship('Message', backref='user', lazy='dynamic')
 
 	def __init__(self, first_name, last_name):
 		self.first_name = first_name
 		self.last_name = last_name
+
+class Message(db.Model):
+
+	__tablename__ = 'messages'
+
+	id = db.Column(db.Integer, primary_key=True)
+	content = db.Column(db.Text)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 
 @app.route('/')
