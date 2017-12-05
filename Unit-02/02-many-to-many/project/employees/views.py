c177be9d12fd5b7e05d82ab33a8c45ac0d22397c
Trevor Hoy <trevorphoy@gmail.com>
2017-12-04 20:56:46 -0800

    full crud on employees

--- Unit-02/02-many-to-many/project/employees/views.py
+++ Unit-02/02-many-to-many/project/employees/views.py
@@ -1,5 +1,6 @@
-from flask import render_template, Blueprint, redirect
+from flask import render_template, Blueprint, redirect, request, url_for
 from project.models import Employee, Department
+from project import db
 
 
 
@@ -12,4 +13,34 @@ employees_blueprint = Blueprint(
 #  Routes for employees go below here
 @employees_blueprint.route('/', methods=['GET','POST'])
 def index():
-	return render_template('employees/index.html')
+	if request.method == 'POST':
+		new_employee = Employee(request.form['name'], request.form['years_at_company'])
+		db.session.add(new_employee)
+		db.session.commit()
+		return redirect(url_for('employees.index'))
+	return render_template('employees/index.html', employees=Employee.query.all())
+
+@employees_blueprint.route('/new')
+def new():
+	return render_template('employees/new.html')
+
+@employees_blueprint.route('/<int:id>/edit')
+def edit(id):
+	employee = Employee.query.get(id)
+	return render_template('employees/edit.html', employee=employee)
+
+@employees_blueprint.route('/<int:id>', methods=['GET','DELETE','PATCH'])
+def show(id):
+	if request.method == b'PATCH':
+		employee = Employee.query.get(id)
+		employee.name = request.form['name']
+		employee.years_at_company = request.form['years_at_company']
+		db.session.add(employee)
+		db.session.commit()
+		return redirect(url_for('employees.index'))
+	if request.method == b'DELETE':
+		employee = Employee.query.get(id)
+		db.session.delete(employee)
+		db.session.commit()
+		return redirect(url_for('employees.index'))
+	return render_template('employees/show.html')
